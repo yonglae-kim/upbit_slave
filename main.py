470e7bd86dc1d1da54648dc0017172ae638ecc9a
Oracle Public Cloud User <opc@instance-20210502-1411.subnet05021025.vcn05021025.oraclevcn.com>
2021-05-03 08:08:11 +0900

    add try-except

--- main.py
+++ main.py
@@ -48,51 +48,58 @@ def check_buy(data):
     if macd['MACDSignal'].iloc[-3] < macd['MACDSignal'].iloc[-2] or macd['MACDSignal'].iloc[-2] > \
             macd['MACDSignal'].iloc[-1]:
         return False
-
+    if macd['MACDSignal'].iloc[-1] > 0:
+        return False
     return True
 
-
 while 1:
-    accounts = apis.get_accounts()
-    my_coins = []
-    has_coin = []
-    for item in accounts:
-        if item['unit_currency'] != 'KRW':
-            continue
-        if item['balance'] == 0:
-            continue
-        if item['currency'] in slave_constants.DO_NOT_TRADING:
-            continue
-        if item['currency'] == "KRW":
-            avail_krw = float(item['balance'])
-            continue
-        my_coins.append(item)
-        has_coin.append("KRW-" + item['currency'])
-
-    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), '보유코인 :', has_coin)
-    for account in my_coins:
-        data = apis.get_candles_minutes("KRW-" + account['currency'], interval=5)
-
-        if check_sell(data, float(account['avg_buy_price']))  or float(data[0]['trade_price']) < float(account['avg_buy_price']) * 0.975:
-            apis.ask_market("KRW-" + account['currency'], float(account['balance']))
-            print("SELL", "KRW-" + account['currency'], account['balance'] + account['currency'],
-                  data[0]['trade_price'])
-            time.sleep(10)
-
-    if avail_krw > 20000 and len(has_coin) < 4:
-        tickers = apis.get_ticker(', '.join(list_krw_market))
-        tickers.sort(key=lambda x: float(x['trade_volume']), reverse=True)
-        time.sleep(5)
-        for ticker in tickers:
-            if ticker['market'] in has_coin:
-                print(ticker['market'], "has already ")
+    try:
+        accounts = apis.get_accounts()
+        my_coins = []
+        has_coin = []
+        for item in accounts:
+            if item['unit_currency'] != 'KRW':
                 continue
-            data = apis.get_candles_minutes(ticker['market'], interval=3)
-            if check_buy(data):
-                apis.bid_price(ticker['market'], avail_krw / 2)
-                print("BUY", ticker['market'], str(avail_krw // 5) + "원", data[0]['trade_price'])
-                avail_krw -= avail_krw // 5
-                break
-            time.sleep(1)
+            if item['balance'] == 0:
+                continue
+            if item['currency'] in slave_constants.DO_NOT_TRADING:
+                continue
+            if item['currency'] == "KRW":
+                avail_krw = float(item['balance'])
+                continue
+            my_coins.append(item)
+            has_coin.append("KRW-" + item['currency'])
 
-    time.sleep(30)
+        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), '보유코인 :', has_coin)
+        for account in my_coins:
+            data = apis.get_candles_minutes("KRW-" + account['currency'], interval=5)
+
+            if check_sell(data, float(account['avg_buy_price'])) or float(data[0]['trade_price']) < float(
+                    account['avg_buy_price']) * 0.975:
+                apis.ask_market("KRW-" + account['currency'], float(account['balance']))
+                print("SELL", "KRW-" + account['currency'], account['balance'] + account['currency'],
+                      data[0]['trade_price'])
+                time.sleep(5)
+
+        if avail_krw > 20000 and len(has_coin) < 4:
+            tickers = apis.get_ticker(', '.join(list_krw_market))
+            tickers.sort(key=lambda x: float(x['trade_volume']), reverse=True)
+            time.sleep(5)
+            for ticker in tickers:
+                if ticker['market'] in has_coin:
+                    print(ticker['market'], "has already ")
+                    continue
+                data = apis.get_candles_minutes(ticker['market'], interval=3)
+                if check_buy(data):
+                    apis.bid_price(ticker['market'], avail_krw / 2)
+                    print("BUY", ticker['market'], str(avail_krw // 5) + "원", data[0]['trade_price'])
+                    avail_krw -= avail_krw // 5
+                    break
+                time.sleep(1)
+
+        time.sleep(30)
+    except KeyboardInterrupt:
+        sys.exit()
+    except:
+        print("...")
+
